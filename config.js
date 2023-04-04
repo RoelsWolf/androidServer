f65c42645c358fc750fc31383bf6a413328a1e86
RoelsWolf <71627605+RoelsWolf@users.noreply.github.com>
2023-04-04 08:20:30 +0200

    test

--- config.js
+++ config.js
@@ -4,6 +4,10 @@ dotenv.config();
 const config = {
     ip: process.env.SERVERIP,
     port: process.env.SERVERPORT,
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 }
 
 export default config;
--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import 'firebase/compat/firestore';
-
+import config from "./config";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  apiKey: config.apiKey,
+  authDomain: config.authDomain,
   projectId: "minecraft-96f09",
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  storageBucket: config.storageBucket,
+  messagingSenderId: config.messagingSenderId,
   appId: "1:931467249550:web:c2f431f148eb31a2c286df",
   measurementId: "G-GPQKMDV1GG"
 };
