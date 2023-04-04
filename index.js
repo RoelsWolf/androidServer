9fbd6188af20f19ded72a170fd83602548ac8b56
RoelsWolf <71627605+RoelsWolf@users.noreply.github.com>
2023-04-04 08:14:28 +0200

    added log statements

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ import { updateDataBase } from "./getData.js";
 const fiveMinutes = 1000 * 60 * 5;
 
 async function runServer(){
+    console.log("Starting server...");
     setInterval(async () => {
+        console.log("Updating database...");
         await updateDataBase();
     }, fiveMinutes);
 }
